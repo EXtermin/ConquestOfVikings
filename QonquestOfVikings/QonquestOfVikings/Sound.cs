10cfea7e3e81ca162415784d94969a4cb1e7d6b0
EXtermin <Damian.meijer@gmail.com>
2017-11-02 22:09:15 +0100

    Added Sound
    +Functions
    +Sound in battle
    +Sound in story

--- QonquestOfVikings/QonquestOfVikings/Sound.cs
+++ QonquestOfVikings/QonquestOfVikings/Sound.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,19 +11,33 @@ namespace QonquestOfVikings
     {
         public void Victory()
         {
-
+            SoundPlayer snd = new SoundPlayer(@"Sound/WinSound.wav");
+            snd.Play();
         }
         public void Defeat()
         {
-
+            SoundPlayer snd = new SoundPlayer(@"Sound/DefeatSound.wav");
+            snd.Play();
+        }
+        public void BattleBackground()
+        {
+            SoundPlayer snd = new SoundPlayer(@"Sound/BackgroundBattleSound.wav");
+            snd.PlayLooping();
+        }
+        public void StoryBackground()
+        {
+            SoundPlayer snd = new SoundPlayer(@"Sound/BackgroundStorySound.wav");
+            snd.Play();
         }
         public void Hit()
         {
-
+            SoundPlayer snd = new SoundPlayer(@"Sound/AttackSound.wav");
+            snd.Play();
         }
         public void LvlUp()
         {
-
+            SoundPlayer snd = new SoundPlayer(@"Sound/ExpSound.wav");
+            snd.Play();
         }
     }
 }
--- QonquestOfVikings/QonquestOfVikings/StoryLine.cs
+++ QonquestOfVikings/QonquestOfVikings/StoryLine.cs
@@ -17,24 +17,28 @@ namespace QonquestOfVikings
         {
             this.player1 = player;
             this.battle = new Battle(player1);
+           
         }
 
         public void Begin()
         {
-            
+            Sound sound = new Sound();
+
+            sound.StoryBackground();
             Console.WriteLine("Welkom {0}", player1.GetPlayerName());
             Thread.Sleep(1000);
-            Console.Clear();
             Console.WriteLine("\n \n \n The story begins...");
-            Thread.Sleep(500);
-            Console.Clear();
+            Thread.Sleep(2000);
             Console.WriteLine("\n A lonely viking... travelling.. across lands... seas... searching...");
-            Thread.Sleep(500);
+            Thread.Sleep(1500);
             Console.WriteLine("\n For a purpose... a purpose to live... and die for...");
-            Thread.Sleep(500);
+            Thread.Sleep(2500);
             Console.Clear();
             Console.WriteLine("But one day... when {0} was walking trough a street... in a town...", player1.GetPlayerName());
+            sound.BattleBackground();
+            Console.WriteLine("\n {0} got suddenly attacked!");
             Thread.Sleep(3000);
+
             Battle();
             
         }
