82181cfac121734b60ccc878006d9832aeed6b5a
RealKayle <RealKayle@gmail.com>
2018-09-05 22:02:12 +0200

    Followupstoryline

--- QonquestOfVikings/QonquestOfVikings/Shop.cs
+++ QonquestOfVikings/QonquestOfVikings/Shop.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace QonquestOfVikings
-{
-    public Shop()
-    {
-        // Lesser MP Potion, MP Potion, Lesser Health Potion, Health Potion, Chain armor, Iron armor, Iron Axe, Torch, 
+//namespace QonquestOfVikings
+//{
+//    public Shop()
+//    {
+//        // Lesser MP Potion, MP Potion, Lesser Health Potion, Health Potion, Chain armor, Iron armor, Iron Axe, Torch, 
         
-    }
-}
+//    }
+//}
 
