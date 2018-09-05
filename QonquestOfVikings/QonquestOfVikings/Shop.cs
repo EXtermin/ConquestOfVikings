26e6d067e0f6594767f1abe6c1eb05f0d755e1e5
RealKayle <RealKayle@gmail.com>
2018-09-05 21:36:19 +0200

    Force Push

--- QonquestOfVikings/QonquestOfVikings/Shop.cs
+++ QonquestOfVikings/QonquestOfVikings/Shop.cs
@@ -1,14 +1,15 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace QonquestOfVikings
-//{
-//    public Shop()
-//    {
-
-//    }
-//}
+namespace QonquestOfVikings
+{
+    public Shop()
+    {
+        // Lesser MP Potion, MP Potion, Lesser Health Potion, Health Potion, Chain armor, Iron armor, Iron Axe, Torch, 
+        
+    }
+}
 
--- QonquestOfVikings/QonquestOfVikings/StoryLine.cs
+++ QonquestOfVikings/QonquestOfVikings/StoryLine.cs
@@ -73,9 +73,55 @@ namespace QonquestOfVikings
 
         public void EndBeginning()
         {
-            Console.Clear();
-            Console.WriteLine("As you walk out of town you are greeded by the town's guard. \n" +
-                "You've known him for most of your life now. He was like a father you never had.");
+            {
+                Console.Clear();
+                Console.WriteLine("\n As you walk out of town you are greeded by the town's guard. ");
+                Thread.Sleep(2000);
+                Console.WriteLine("\n You've known him for most of your life now. He was like the father you never had.");
+                Thread.Sleep(2000);
+                Console.WriteLine("\n Growing up alone wasn't easy for you but he always helped you along your way. His name was Asgier.");
+                Thread.Sleep(2000);
+                Console.WriteLine("\n As you walk by Asgier, you both node and you dissapear onto a path. You take the path north through a gaint forest.");
+                Thread.Sleep(2000);
+                Console.WriteLine("\n While you are going deeper into the forest you start hearing noises behind the tree's. \n \n ");
+
+                Console.WriteLine("1) Look behind tree's");
+                Console.WriteLine("2) Keep walking");
+
+            }
+            
+       
+
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                Console.Clear();
+                Thread.Sleep(2000);
+                Console.WriteLine("\n  As you look behind the trees you see a small goblin running at you. He is ready to fight!");  // niet te ver van de town dus kan terug rennen en om guard's hulp vragen
+                Thread.Sleep(3500);
+                Console.Clear();
+                Battle();
+            }
+            else if (choice == "2")
+            {
+                Console.Clear();
+                Thread.Sleep(2000);
+                Console.WriteLine("\n  You shake the thought and continue walking. As you start to stop hearing the noises you step in something.\n");
+                Thread.Sleep(2000);
+                Console.WriteLine(" ....");
+                Thread.Sleep(2000);
+                Console.WriteLine(" ....");
+                Thread.Sleep(2000);
+                Console.WriteLine(" It's a trap! your foot is stuck in a bear trap but its to weak to hurt you. As you look around you get " +
+                    "\n attacked by a bandit.");
+                Thread.Sleep(3500);
+                Console.Clear();
+                Battle();
+            }
+               
+               
+            
+
         }
 
         public void Battle()
