af2b80c3ebfcfeff8c9c408ea87e8f9b35666855
EXtermin <Damian.meijer@gmail.com>
2018-09-05 22:10:25 +0200

    Start/End
    Ik denk dat je hier mee wel verder kan kwa story line? als er weer events moet komen just comment them!

--- QonquestOfVikings/QonquestOfVikings/FollowUpStoryline.cs
+++ QonquestOfVikings/QonquestOfVikings/FollowUpStoryline.cs
@@ -8,5 +8,30 @@ namespace QonquestOfVikings
 {
     class FollowUpStoryline
     {
+        Player player1;
+        Battle battle;
+        public FollowUpStoryline(Player player)
+        {
+            this.player1 = player;
+            this.battle = new Battle(player1);
+           
+        }
+        public void FollowUpBegin()
+        {
+            
+        }
+         public void FollowUpMiddle()
+        {
+            
+        }
+         public void FollowUpEnd()
+        {
+            
+        }
+        public void Battle()
+        {
+            //Deze kan verandert worden ivm andere vijanden
+            battle.battle();
+        }
     }
 }
