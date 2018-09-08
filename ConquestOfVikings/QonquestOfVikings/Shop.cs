55db3366b711bdf0f3982447920f16a7478bd360
EXtermin <Damian.meijer@gmail.com>
2018-09-08 22:15:26 +0200

    Start of shop

--- ConquestOfVikings/QonquestOfVikings/Shop.cs
+++ ConquestOfVikings/QonquestOfVikings/Shop.cs
@@ -1,15 +1,29 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace QonquestOfVikings
-//{
-//    public Shop()
-//    {
-//        // Lesser MP Potion, MP Potion, Lesser Health Potion, Health Potion, Chain armor, Iron armor, Iron Axe, Torch, 
-        
-//    }
-//}
+namespace QonquestOfVikings
+{
+    class Shop
+    {
+        Player player1;
+        public Shop(Player player)
+        {
+            this.player1 = player;
+        }
+
+        public void Transaction(string item)
+        {
+            
+            StoryLine temp = new StoryLine(player1);
+            temp.townShop();
+        }
+        // Lesser MP Potion, MP Potion, Lesser Health Potion, Health Potion, Chain armor, Iron armor, Iron Axe, Torch, 
+
+
+
+    }
+}
 
