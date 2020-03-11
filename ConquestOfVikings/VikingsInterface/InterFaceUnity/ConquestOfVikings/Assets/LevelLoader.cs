4b7283af0b705f08ad6385957e14e10090ec5a74
Damian <Damian.meijer@gmail.com>
2020-03-11 10:35:42 +0100

    Unity update

---
+++ ConquestOfVikings/VikingsInterface/InterFaceUnity/ConquestOfVikings/Assets/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transistion;
+
+    public float transistionTime = 1f;
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            LoadNextLevel();
+        }
+    }
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transistion.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transistionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ ConquestOfVikings/VikingsInterface/InterFaceUnity/ConquestOfVikings/Assets/Scenes/BattleTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleTrigger : MonoBehaviour
+{
+    public string level = "";
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
